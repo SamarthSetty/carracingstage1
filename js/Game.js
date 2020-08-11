125cca728d253f4cece8313570515cace7f921e0
SamarthSetty <samarth.setty30@gmail.com>
2020-08-11 20:49:12 +0530

    gave different player values

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,42 @@ class Game {
     });
   }
 
-  start(){
+async  start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountref=await database.ref('playerCount').once("value")
+      if(playerCountref.exists()){
+        playerCount=playerCountref.val()
+        player.getCount()
+      }
+     
       form = new Form()
       form.display();
     }
   }
+
+  play(){
+    form.hide()
+    textSize(30)
+    text("game start",120,100);
+    Player.getPlayerInfo()
+    if(allPlayers!== undefined){
+      var y=130;
+      for(var plr in allPlayers){
+        if(plr==="player"+player.index){
+          fill("red");
+          }
+           else 
+          fill("black");
+          y=y+20;
+          textSize(15);
+          text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,y);
+      }
+    }
+    if(keyDown(UP_ARROW)&&player.index!== null){
+  player.distance=player.distance+50
+  player.update()    
+    }
+  }
 }
+
